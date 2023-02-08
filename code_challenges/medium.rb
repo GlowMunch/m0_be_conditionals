6ffd18f933e410d95eb2125bd4cc5d1d135c79ed
Blaine Glasgow <glasgowb@outlook.com>
2023-02-07 22:05:19 -0800

    Fix Medium

--- code_challenges/medium.rb
+++ code_challenges/medium.rb
@@ -1,12 +1,12 @@
-good_driving_record = false
-age = 18
+good_driving_record = true
+age = 30
 
 if good_driving_record == true && age >= 25
     p "You get a discount on your rental"
-elsif good_driving_record == true || age >= 25
+elsif age >= 25 && good_driving_record == false
     p "You can rent at full price"
-elsif good_driving_record == false && age < 25
+elsif age >= 16 || good_driving_record == true
     p "You can rent, but need a co-signer"
 elsif
-    p "Sorry, you cannot rent a car"
+    p "Sorry, you cannot rent a car. Have some soda, kid"
 end
