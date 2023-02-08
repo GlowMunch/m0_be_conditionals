49c88af19680a4c402fea52403dae4bc70638e9d
Blaine Glasgow <glasgowb@outlook.com>
2023-02-07 17:23:24 -0800

    Add spicy.md

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,11 @@
+num = 11
+   if num % 3 == 0 && num % 5
+      puts "fizbuz"
+   elsif num % 3 == 0
+      puts "fiz"
+   elsif num % 5 == 0
+      puts "buz"
+   elsif
+      puts "Nada, your number is #{num}"
+ end
+ 
